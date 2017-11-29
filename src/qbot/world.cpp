933bed25ad12ca0e1eac76b2b9b1691139b27162
rosasam <samuel.rosas@aalto.fi>
2017-11-29 18:32:59 +0200

    Work on FooTest

--- src/qbot/world.cpp
+++ src/qbot/world.cpp
@@ -4,6 +4,7 @@
 #include <Box2D/Box2D.h>
 #include <stdio.h>
 #include <iostream>
+#include <iomanip>
 
 #include "world.hpp"
 #include "wurm.hpp"
@@ -17,11 +18,11 @@
     groundBodyDef.position.Set(0.0f, -10.0f);
     b2Body* groundBody = world->CreateBody(&groundBodyDef);
     b2PolygonShape groundBox;
-    groundBox.SetAsBox(50.0f, 10.0f);
+    groundBox.SetAsBox(50.0f, 1.0f);
     groundBody->CreateFixture(&groundBox, 0.0f);
     
     // WURMY THE WURM
-    wurmy = new Wurm(4, world);
+    wurmy = new Wurm(3, world);
   }
   
   World::~World() { };
@@ -32,18 +33,41 @@
     int32 velocityIterations = 8;
     int32 positionIterations = 3;
     
+    // LET'S APPLY A FORCE
+    wurmy->GetBodies()[0]->ApplyLinearImpulse(b2Vec2(-10,300), b2Vec2(0,0));
+    
+    // TEST ITERATIONS
     std::cout << "Starting" << std::endl;
-    for (int32 i = 0; i < 60; ++i) {
-      world->Step(timeStep, velocityIterations, positionIterations);
-      //std::cout << wurmy->GetBodies().size() << std::endl;
-      for (auto body : wurmy->GetBodies()) {
-        b2Vec2 position = body->GetPosition();
-        float32 angle = body->GetAngle();
-        std::cout << position.x << position.y << angle << std::endl;
-      }
-      //std::cout << std::endl;
-      world->ClearForces();
+    for (auto body : wurmy->GetBodies()) {
+      b2Vec2 position = body->GetPosition();
+      float32 angle = body->GetAngle();
+      std::cout << std::setw(15) << position.x << 
+                   std::setw(15) << position.y << 
+                   std::setw(15) << angle << std::endl;
     }
+    for (int32 i = 0; i < 600; ++i) {
+      world->Step(timeStep, velocityIterations, positionIterations);
+      world->ClearForces();
+      if(i%60 == 0) {
+        std::cout << "\nSecond " << i/60 << " starting" << std::endl;
+        for (auto body : wurmy->GetBodies()) {
+          b2Vec2 position = body->GetPosition();
+          float32 angle = body->GetAngle();
+          std::cout << std::setw(15) << position.x << 
+                       std::setw(15) << position.y << 
+                       std::setw(15) << angle << std::endl;
+        }
+      }
+    }
+    std::cout << "\n" << std::endl;
+    for (auto body : wurmy->GetBodies()) {
+      b2Vec2 position = body->GetPosition();
+      float32 angle = body->GetAngle();
+      std::cout << std::setw(15) << position.x << 
+                   std::setw(15) << position.y << 
+                   std::setw(15) << angle << std::endl;
+    }
+    // THE END
     std::cout << "The End" << std::endl;
   }
     
--- src/qbot/wurm.cpp
+++ src/qbot/wurm.cpp
@@ -7,12 +7,12 @@
 
   // CONSTRUCTOR
   Wurm::Wurm(int jointCount, b2World *world) {
-    std::vector<b2Joint*> joints_(jointCount);
+    //std::vector<b2Joint*> joints_(jointCount);
     //std::vector<b2Body*> bodies_(jointCount+1);
 
     // BODY DEFINITIONS
     b2BodyDef bodyPartDef;
-    bodyPartDef.position.Set(0.0f, 0.0f);
+    bodyPartDef.position.Set(0.0f, 20.0f);
     bodyPartDef.type = b2_dynamicBody;
     bodyPartDef.linearDamping = 0.0f;
     bodyPartDef.angularDamping = 0.01f;
@@ -21,8 +21,7 @@
 
     // SHAPE
     b2PolygonShape bodyPartShape;
-    bodyPartShape.SetAsBox(1, 5);
-
+    bodyPartShape.SetAsBox(5, 1);
     // BODY FIXTURE //TODO: Shape
     b2FixtureDef bodyPartFixture;
     bodyPartFixture.density = 1.0f;
@@ -31,15 +30,17 @@
     // CREATE WURM
     bodies_.push_back(world->CreateBody(&bodyPartDef));
     bodies_.back()->CreateFixture(&bodyPartFixture);
-    for(int i = 0; i < jointCount; i++) {
-      bodyPartDef.position.Set(1*i, 0.0f);
+    for(int i = 0; i < jointCount; i++) {    
+      bodyPartDef.position.Set(10*(i+1), 0.0f);
       bodies_.push_back(world->CreateBody(&bodyPartDef));
       bodies_.back()->CreateFixture(&bodyPartFixture);
-
-      /*b2RevoluteJointDef jointDef;
-      jointDef.Initialize(bodies_[i], bodies_[i+1],
-                          bodies_[i]->GetWorldCenter());
-      joints_[i] = world->CreateJoint(&jointDef);*/
+      
+      // MOTOR JOINT DEF
+      b2MotorJointDef jointDef;
+      jointDef.Initialize(bodies_[i], bodies_[i+1])      
+		  jointDef.maxForce = 10.0f;
+		  jointDef.maxTorque = 10.0f;
+      joints_.push_back(world->CreateJoint(&jointDef));
     }
   }
 
@@ -60,7 +61,9 @@
   // 1  for clockwise
   // -1 for counter-clockwise
   // 0  to stop rotation
-  void Wurm::MoveJoint(int joint_index, int direction) {
+  void Wurm::MoveJoint(int joint_index, int direction, float offset) {
+    b2MotorJoint joint = joints_[joint_index];
+    joint->SetAngularOffset(offset);
     return; // TODO
   }
 
