678eb10d6ba6a64b468184333ba2e1f31547e953
Eretzu <m.hemilae@gmail.com>
2017-11-29 18:08:14 +0200

    Managed to compile testbed but newer version is needed for box2d

--- src/qbot/wurm.cpp
+++ src/qbot/wurm.cpp
@@ -7,7 +7,7 @@
 
   // CONSTRUCTOR
   Wurm::Wurm(int jointCount, b2World *world) {
-    std::vector<b2Joint*> joints_(jointCount);
+    //std::vector<b2Joint*> joints_(jointCount);
     //std::vector<b2Body*> bodies_(jointCount+1);
     
     // BODY DEFINITIONS
@@ -21,25 +21,27 @@
     
     // SHAPE
     b2PolygonShape bodyPartShape;
-    bodyPartShape.SetAsBox(0.01f, 0.05);
+    bodyPartShape.SetAsBox(5, 1);
     
     // BODY FIXTURE //TODO: Shape
     b2FixtureDef bodyPartFixture;
-    //bodyPartFixture.SetDensity(1.0f);
+    bodyPartFixture.density = 1.0f;
     bodyPartFixture.shape = &bodyPartShape;
     
     // CREATE WURM
     bodies_.push_back(world->CreateBody(&bodyPartDef));
     bodies_.back()->CreateFixture(&bodyPartFixture);
     for(int i = 0; i < jointCount; i++) {    
-      bodyPartDef.position.Set(0.0f*i, 0.0f);
+      bodyPartDef.position.Set(10*(i+1), 0.0f);
       bodies_.push_back(world->CreateBody(&bodyPartDef));
       bodies_.back()->CreateFixture(&bodyPartFixture);
       
-      /*b2RevoluteJointDef jointDef;
-      jointDef.Initialize(bodies_[i], bodies_[i+1], 
-                          bodies_[i]->GetWorldCenter());
-      joints_[i] = world->CreateJoint(&jointDef);*/
+      // MOTOR JOINT DEF
+      b2MotorJointDef jointDef;
+      jointDef.Initialize(bodies_[i], bodies_[i+1])      
+		  jointDef.maxForce = 10.0f;
+		  jointDef.maxTorque = 10.0f;
+      joints_.push_back(world->CreateJoint(&jointDef));
     }
   }
   
@@ -60,7 +62,9 @@
   // 1  for clockwise
   // -1 for counter-clockwise
   // 0  to stop rotation
-  void Wurm::MoveJoint(int joint_index, int direction) {
+  void Wurm::MoveJoint(int joint_index, int direction, float offset) {
+    b2MotorJoint joint = joints_[joint_index];
+    joint->SetAngularOffset(offset);
     return; // TODO
   }
 
