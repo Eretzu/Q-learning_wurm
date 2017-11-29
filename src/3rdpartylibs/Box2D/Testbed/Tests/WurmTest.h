3810ea6dc9ecdb4cf2db3855a06e7f4c67dde295
rosasam <samuel.rosas@aalto.fi>
2017-11-29 17:08:14 +0200

    Add some missing files to wurmtest (this is a dirty hack)

---
+++ src/3rdpartylibs/Box2D/Testbed/Tests/WurmTest.h
@@ -0,0 +1,26 @@
+#ifndef WURMTEST_H
+#define WURMTEST_H
+
+#include "qbot/wurm.hpp"
+
+class WurmTest : public Test {
+public:
+  WurmTest() {
+    Wurm* w = new Wurm(3, m_world);
+  }
+
+  void Step(Settings* settings) {
+    //run the default physics and rendering
+    Test::Step(settings);
+
+    //show some text in the main screen
+    //m_debugDraw.DrawString(5, m_textLine, "Now we have a foo test");
+    //m_textLine += 15;
+  }
+
+  static Test* Create() {
+    return new WurmTest;
+  }
+};
+
+#endif
