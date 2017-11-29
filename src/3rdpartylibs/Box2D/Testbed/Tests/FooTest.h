d8f08ec5c0eeaf578edaf66753a5b80c8eb4d4ba
rosasam <samuel.rosas@aalto.fi>
2017-11-29 17:12:11 +0200

    Physics test

--- src/3rdpartylibs/Box2D/Testbed/Tests/FooTest.h
+++ src/3rdpartylibs/Box2D/Testbed/Tests/FooTest.h
@@ -1,39 +1,48 @@
 #ifndef FOOTEST_H
 #define FOOTEST_H
 
-class FooTest: public Test {
+#define DEGTORAD 0.0174532925199432957f
+#define RADTODEG 57.295779513082320876f
+
+class FooTest : public Test {
 public:
   FooTest() {
-    b2BodyDef myBodyDef;
-    myBodyDef.type = b2_dynamicBody; //this will be a dynamic body
-    myBodyDef.position.Set(0, 20); //set the starting position
-    myBodyDef.angle = 0; //set the starting angle
-    b2Body* dynamicBody = m_world->CreateBody(&myBodyDef);
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0, 20); //middle
+    b2Body* dynamicBody = m_world->CreateBody(&bodyDef);
 
-    b2PolygonShape boxShape;
-    boxShape.SetAsBox(1,1);
+    // Prepare shape definition
+    b2PolygonShape polygonShape;
+    b2EdgeShape edgeShape;
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &polygonShape;
+    fixtureDef.density = 1;
+    fixtureDef.friction = 1;
+    fixtureDef.restitution = 1;
 
-    b2FixtureDef boxFixtureDef;
-    boxFixtureDef.shape = &boxShape;
-    boxFixtureDef.density = 1;
-    boxFixtureDef.restitution = 0.9;
-    dynamicBody->CreateFixture(&boxFixtureDef);
-    dynamicBody->SetTransform(b2Vec2(10, 20), 3);
-    dynamicBody->SetLinearVelocity( b2Vec2( 2, -10 ) );
+    for (int i = 0; i < 4; i++) {
+      b2Vec2 pos( sinf(i*90*DEGTORAD), cosf(i*90*DEGTORAD));
+      polygonShape.SetAsBox(1, 1, pos, 0);
+      dynamicBody->CreateFixture(&fixtureDef);
+    }
 
-    myBodyDef.type = b2_staticBody; //this will be a static body
-    myBodyDef.position.Set(0, 5); //slightly lower position
-    b2Body* staticBody = m_world->CreateBody(&myBodyDef); //add body to world
-    boxShape.SetAsBox(100,1);
+    bodyDef.type = b2_staticBody;
+    bodyDef.position.Set(0, 0); //middle, bottom
+    b2Body* staticBody = m_world->CreateBody(&bodyDef);
 
-    staticBody->CreateFixture(&boxFixtureDef); //add fixture to body
+    fixtureDef.shape = &edgeShape;
+    edgeShape.Set( b2Vec2(-25,0), b2Vec2(25,3) ); //slightly sloped
+    staticBody->CreateFixture(&fixtureDef);
   }
 
   void Step(Settings* settings) {
+    //run the default physics and rendering
     Test::Step(settings);
 
-    m_debugDraw.DrawString(5, m_textLine, "Now we have a foo test");
-    m_textLine += 15;
+    //show some text in the main screen
+    //m_debugDraw.DrawString(5, m_textLine, "Now we have a foo test");
+    //m_textLine += 15;
   }
 
   static Test* Create() {
