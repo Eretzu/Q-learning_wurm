83303bc6b282eee68fa71c2c281830f3057f0aca
rosasam <samuel.rosas@aalto.fi>
2017-12-04 17:24:23 +0200

    Change box2d search path

--- src/qbot/main.cpp
+++ src/qbot/main.cpp
@@ -1,9 +1,13 @@
 #include "world.hpp"
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
 
 int main(int argc, char** argv) {
   World moi = World();
-  
-  moi.start();    
-  
+
+  moi.start();
+
+
+
   return 0;
 }
