1f1a2264dd867265edd9203aa5cad5114018255f
Eretzu <m.hemilae@gmail.com>
2017-12-06 11:43:34 +0200

    SFML Test created and compiled. It works

--- src/qbot/main.cpp
+++ src/qbot/main.cpp
@@ -1,13 +1,26 @@
 #include "world.hpp"
 #include <SFML/Graphics.hpp>
-#include <Box2D/Box2D.h>
+//#include <Box2D/Box2D.h>
 
-int main(int argc, char** argv) {
-  World moi = World();
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-  moi.start();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-
-  return 0;
+    return 0;
 }
