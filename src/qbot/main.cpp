abac58996bd20af6afb503a88bda74ef7a1ac6e5
rosasam <samuel.rosas@aalto.fi>
2017-12-09 15:59:19 +0200

    Move drawing functions to own class

--- src/qbot/main.cpp
+++ src/qbot/main.cpp
@@ -1,22 +1,11 @@
 #include "world.hpp"
 #include "brains.hpp"
+#include "draw.hpp"
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
 
-const float SCALE = 30.f;
+const float SCALE = 10.f;
 sf::Color boxColor = sf::Color::Black;
-
-// sf::ConvexShape CreateSfPolygon(b2PolygonShape* b2Polygon) {
-//     int vertexCount = b2Polygon->GetVertexCount();
-//
-//     sf::ConvexShape polygon;
-//
-//     for (int i = 0; i < vertexCount; ++i) {
-//       polygon.setPoint(i, sf::Vector2f(b2Polygon->GetVertex(i).x * SCALE, b2Polygon->GetVertex(i).y * SCALE));
-//       polygon.setFillColor(boxColor);
-//     }
-//     return polygon;
-// }
 
 void CreateGround(b2World& world, float X, float Y) {
     b2BodyDef BodyDef;
@@ -49,8 +38,8 @@ void CreateBox(b2World& world, int MouseX, int MouseY) {
 
 
 int main() {
-    sf::View view1(sf::Vector2f(0, 0), sf::Vector2f(1800, 600));
-    sf::RenderWindow window(sf::VideoMode(1800, 600), "Test");
+    sf::View view1(sf::Vector2f(0, 0), sf::Vector2f(1400, 600));
+    sf::RenderWindow window(sf::VideoMode(1400, 600), "Test");
     window.setFramerateLimit(60);
     window.setView(view1);
 
@@ -60,15 +49,15 @@ int main() {
     World* worldy = new World();
     b2World world = *(worldy->GetWorld());
     Brains* b = new Brains(24, &world);
-
+    Draw draw;
     //CreateGround(world, 400.f, 500.f);
-    
+
     //int i = 0;
     while (window.isOpen()) {
         auto xyy = b->GetWurm()->GetWurmPosition();
         view1.setCenter(xyy->x*SCALE, xyy->y*SCALE);
         window.setView(view1);
-        
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -85,62 +74,13 @@ int main() {
         // Simulate the world
         world.Step(1/60.f, 8, 3);
         b->Think();
-        
-        
+
+
 
         // Draw here
         window.clear(sf::Color::White);
 
-        for (b2Body* body = world.GetBodyList(); body; body = body->GetNext()) {
-          if (body->GetType() == b2_dynamicBody) {
-            for (b2Fixture* fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext()) {
-              b2Shape::Type shapeType = fixture->GetType();
-                /*if ( shapeType == b2Shape::e_circle ) {
-                    b2CircleShape* circle = (b2CircleShape*)fixture->GetShape();
-                }*/
-                /*else*/ if ( shapeType == b2Shape::e_polygon ) {
-                    //b2PolygonShape* polygon = (b2PolygonShape*)fixture->GetShape();
-
-                    //auto p = CreateSfPolygon(polygon);
-                    sf::RectangleShape p(sf::Vector2f(10.f*SCALE, 2.f*SCALE));
-                    p.setFillColor(sf::Color(50, 50, 50));
-                    p.setOrigin(10.f*SCALE/2, 2.f*SCALE/2);
-                    p.setPosition(SCALE * body->GetPosition().x, SCALE * body->GetPosition().y);
-                    p.setRotation(body->GetAngle() * 180/b2_pi);
-                    p.setOutlineThickness(1.f);
-                    p.setOutlineColor(sf::Color::Black);
-                    window.draw(p);
-                } // pituus 10, leveys 2
-            }
-            // sf::RectangleShape r(sf::Vector2f(32.f, 32.f));
-            // r.setOrigin(32.f/2, 32.f/2);
-            // r.setFillColor(sf::Color::Black);
-            // r.setPosition(SCALE * body->GetPosition().x, SCALE * body->GetPosition().y);
-            // r.setRotation(body->GetAngle() * 180/b2_pi);
-            // window.draw(r);
-          }
-          else {
-            for (b2Fixture* fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext()) {
-              b2Shape::Type shapeType = fixture->GetType();
-                /*if ( shapeType == b2Shape::e_circle ) {
-                    b2CircleShape* circle = (b2CircleShape*)fixture->GetShape();
-                }*/
-                /*else*/ if ( shapeType == b2Shape::e_polygon ) {
-                    //b2PolygonShape* polygon = (b2PolygonShape*)fixture->GetShape();
-
-                    //auto p = CreateSfPolygon(polygon);
-                    sf::RectangleShape p(sf::Vector2f(80000.f, 16));
-                    p.setFillColor(sf::Color(50, 50, 50));
-                    p.setOrigin(80000.f/2, 16.f/2);
-                    p.setPosition(SCALE * body->GetPosition().x, SCALE * body->GetPosition().y);
-                    p.setRotation(body->GetAngle() * 180/b2_pi);
-                    p.setOutlineThickness(1.f);
-                    p.setOutlineColor(sf::Color::Black);
-                    window.draw(p);
-                } // pituus 10, leveys 2
-            }
-          }
-        }
+        draw.DrawShapes(window, world);
         window.setView(view1);
         window.display();
     }
